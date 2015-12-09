840091ce259185a5ba389100430ddcdd8d221e3a
Jonas Weber <mail@jonasw.de>
2015-12-09 18:08:06 +0100

    Actually wrap

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 
   IFS=$'\t' read -ra FIELDS <<< "$line"
   PACKAGER=${FIELDS[0]}
-  PACKAGE=${FIELDS[1]} BINARY=${FIELDS[2]} ./involucro -f ${PACKAGER}.lua build
+  PACKAGE=${FIELDS[1]} BINARY=${FIELDS[2]} ./involucro -f ${PACKAGER}.lua build package clean
 
   if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
     docker push thriqon/mulled:${FIELDS[1]}
