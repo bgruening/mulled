5c3200ef9f2dba1cf9b245d265663c020028cdb1
Jonas Weber <mail@jonasw.de>
2015-12-11 14:55:58 +0100

    Using digest from upload

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ function jsonForImage {
     echo -n "\"published\": true, "
   fi
   echo -n "\"size\": \"$(docker inspect -f "{{.VirtualSize}}" $REPO | numfmt --to=iec-i --suffix=B)\", "
-  echo -n "\"checksum\": \"$(docker save $REPO | sha256sum - | cut -d' ' -f 1)\", "
+  echo -n "\"checksum\": \"$(cat info/digest.txt)\", "
   echo -n "\"buildurl\": \"https://travis-ci.org/thriqon/mulled/builds/$TRAVIS_BUILD_ID\", "
   echo -n "\"packager\": \"$PACKAGER\"}"
 }
@@ -29,18 +29,23 @@ function buildPackage {
 
   PACKAGE=$PACKAGE BINARY=$BINARY ADDITIONAL_PACKAGES=$ADDITIONAL_PACKAGES ./involucro -f ${PACKAGER}.lua build package
 
-  POST_FILENAME=page/_posts/2015-01-01-$PACKAGE.html
-  DATA_FILENAME=page/_data/$PACKAGE.json
-  jsonForImage $PACKAGE $PACKAGER | jq . > $DATA_FILENAME
-  (
+  if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
+    docker push thriqon/mulled:$PACKAGE > info/push.log
+    grep digest info/push.log | cut -d " " -f 3 > info/digest.txt
+    echo "UPLOAD"
+    cat info/push.log
+    echo "DIGEST"
+    cat info/digest.txt
+
+    POST_FILENAME=page/_posts/2015-01-01-$PACKAGE.html
+    DATA_FILENAME=page/_data/$PACKAGE.json
+    jsonForImage $PACKAGE $PACKAGER | jq . > $DATA_FILENAME
+    (
     echo "---"
     echo "layout: post"
     echo "image: $PACKAGE"
     echo "---"
-  ) > $POST_FILENAME
-
-  if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-    docker push thriqon/mulled:$PACKAGE | cat
+    ) > $POST_FILENAME
   fi
   
   PACKAGE=$PACKAGE BINARY=$BINARY ADDITIONAL_PACKAGES=$ADDITIONAL_PACKAGES ./involucro -f ${PACKAGER}.lua clean
@@ -53,8 +58,9 @@ function buildPackage {
   buildPackage ${FIELDS[0]} ${FIELDS[1]} ${FIELDS[2]} ${FIELDS[3]}
 done) < packages.tsv
 
-git config --global user.name "Travis BuildBot"
-git config --global user.email "travis@mulled.jonasw.de"
+if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
+  git config --global user.name "Travis BuildBot"
+  git config --global user.email "travis@mulled.jonasw.de"
 
-(cd page ; git add . ; git commit -m "results for $TRAVIS_BUILD_NR" -m "" -m "commit range $TRAVIS_COMMIT_RANGE" -m "travis build https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID" ; git push -q origin gh-pages > /dev/null 2>&1)
-
+  (cd page ; git add . ; git commit -m "results for $TRAVIS_BUILD_NR" -m "" -m "commit range $TRAVIS_COMMIT_RANGE" -m "travis build https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID" ; git push -q origin gh-pages > /dev/null 2>&1)
+fi
